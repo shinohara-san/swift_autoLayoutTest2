c63a0d071a083972ca3a99a566df4bfcbb52b5f8
shinohara-san <someiyoshino0809@gmail.com>
2020-06-04 14:27:37 +0900

    Initial Commit

---
+++ swift_autoLayoutTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_autoLayoutTest2
+//
+//  Created by Yuki Shinohara on 2020/06/04.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
